2f11b387d606c6523f01790e5573f3e624e4903b
Tomona Nanase <nanase@users.noreply.github.com>
2014-09-10 19:58:47 +0900

    FontLoaderクラスを追加
    from sitrine

---
+++ Nagisa/Graphics/FontLoader.cs
@@ -0,0 +1,116 @@
+﻿/* Nagisa - 2D Game Library with OpenTK */
+
+/* LICENSE - The MIT License (MIT)
+ 
+Copyright (c) 2014 Tomona Nanase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
+using System.Drawing;
+using System.Drawing.Text;
+
+namespace Nagisa.Graphics
+{
+    /// <summary>
+    /// ファイルからフォントをロードするためのローダです。
+    /// </summary>
+    public class FontLoader : IDisposable
+    {
+        #region -- Private Fields --
+        private PrivateFontCollection fontCollection;
+        private FontFamily family;
+
+        private bool disposed = false;
+        #endregion
+
+        #region -- Public Properties --
+        /// <summary>
+        /// フォントから読み込みに成功したフォントファミリを取得します。
+        /// </summary>
+        public FontFamily Family { get { return this.family; } }
+        #endregion
+
+        #region -- Constructors --
+        /// <summary>
+        /// 読み込むファイルを指定して新しい FontLoader クラスのインスタンスを初期化します。
+        /// </summary>
+        /// <param name="filename">読み込まれるフォントファイル。</param>
+        public FontLoader(string filename)
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+                throw new ArgumentNullException("filename");
+
+            this.fontCollection = new PrivateFontCollection();
+            this.fontCollection.AddFontFile(filename);
+
+            if (this.fontCollection.Families.Length == 0)
+                throw new Exception("指定されたファイルからフォントが見つかりません。");
+
+            this.family = this.fontCollection.Families[0];
+        }
+        #endregion
+
+        #region -- Public Methods --
+        /// <summary>
+        /// このオブジェクトで使用されているリソースを解放します。
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        #region -- Protected Methods --
+        /// <summary>
+        /// このオブジェクトによって使用されているアンマネージリソースを解放し、オプションでマネージリソースも解放します。
+        /// </summary>
+        /// <param name="disposing">
+        /// マネージリソースとアンマネージリソースの両方を解放する場合は true。アンマネージリソースだけを解放する場合は false。
+        /// </param>
+        protected void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    if (this.family != null)
+                        this.family.Dispose();
+
+                    if (this.fontCollection != null)
+                        this.fontCollection.Dispose();
+                }
+
+                this.family = null;
+                this.fontCollection = null;
+
+                this.disposed = true;
+            }
+        }
+        #endregion
+
+        #region -- Destructors --
+        ~FontLoader()
+        {
+            this.Dispose(false);
+        }
+        #endregion
+    }
+}
