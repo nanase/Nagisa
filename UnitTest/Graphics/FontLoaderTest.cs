af5be53e228c54f45fee8dd9833a399b7ab20241
Tomona Nanase <nanase@users.noreply.github.com>
2014-09-16 23:23:33 +0900

    FontLoaderTestクラスを追加

---
+++ UnitTest/Graphics/FontLoaderTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nagisa.Graphics;
+
+namespace UnitTest.Graphics
+{
+    [TestClass]
+    public class FontLoaderTest
+    {
+        [TestMethod]
+        public void ConstructorTest1()
+        {
+            using (FontLoader font = new FontLoader("./resource/nanodigi.ttf"))
+            {
+                Assert.AreEqual(1, font.Families.Count);
+                Assert.AreEqual("NanoDigi", font.Families[0].Name);
+            }
+        }
+
+        [TestMethod]
+        public void ConstructorTest2()
+        {
+            using (Stream stream = new FileStream("./resource/nanodigi.ttf", FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (FontLoader font = new FontLoader(stream, (int)stream.Length))
+            {
+                Assert.AreEqual(1, font.Families.Count);
+                Assert.AreEqual("NanoDigi", font.Families[0].Name);
+            }
+        }
+    }
+}
