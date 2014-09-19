9526c5319bc38c82c1fbbedf59a92c27db831d75
Tomona Nanase <nanase@users.noreply.github.com>
2014-09-19 23:32:31 +0900

    FontLoaderTestクラスにテストメソッドを追加

--- UnitTest/Graphics/FontLoaderTest.cs
+++ UnitTest/Graphics/FontLoaderTest.cs
@@ -19,6 +19,15 @@ namespace UnitTest.Graphics
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorExceptionTest1()
+        {
+            using (FontLoader font = new FontLoader(null)) { }
+
+            Assert.Fail();
+        }
+
+        [TestMethod]
         public void ConstructorTest2()
         {
             using (Stream stream = new FileStream("./resource/nanodigi.ttf", FileMode.Open, FileAccess.Read, FileShare.Read))
@@ -27,6 +36,26 @@ namespace UnitTest.Graphics
                 Assert.AreEqual(1, font.Families.Count);
                 Assert.AreEqual("NanoDigi", font.Families[0].Name);
             }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorException1Test2()
+        {
+            using (Stream stream = new FileStream("./resource/nanodigi.ttf", FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (FontLoader font = new FontLoader(null, (int)stream.Length)) { }
+
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ConstructorException2Test2()
+        {
+            using (Stream stream = new FileStream("./resource/nanodigi.ttf", FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (FontLoader font = new FontLoader(stream, -1)) { }
+
+            Assert.Fail();
         }
 
         [TestMethod]
@@ -45,6 +74,33 @@ namespace UnitTest.Graphics
                     loader.Dispose();
                     Assert.IsTrue(loader.IsDisposed);
                     loader = null;
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void FamiliesExceptionTest()
+        {
+            FontLoader loader = null;
+            try
+            {
+                loader = new FontLoader("./resource/nanodigi.ttf");
+            }
+            finally
+            {
+                if (loader != null)
+                {
+                    loader.Dispose();
+
+                    // Fire!
+                    var families = loader.Families;
+
+                    loader = null;
+                }
+                else
+                {
+                    Assert.Fail();
                 }
             }
         }
