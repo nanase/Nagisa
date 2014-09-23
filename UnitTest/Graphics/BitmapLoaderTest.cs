e39bee276e20cbfe18bb2fd8cce7e4d677661119
Tomona Nanase <nanase@users.noreply.github.com>
2014-09-23 21:54:11 +0900

    BitmapLoaderTest.csを追加

---
+++ UnitTest/Graphics/BitmapLoaderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nagisa.Graphics;
+
+namespace UnitTest.Graphics
+{
+    [TestClass]
+    public class BitmapLoaderTest
+    {
+        private Bitmap smallBitmap;
+        private Bitmap writtenBitmap;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.smallBitmap = new Bitmap(32, 32);
+            this.writtenBitmap = new Bitmap(32, 32);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            this.smallBitmap.Dispose();
+            this.writtenBitmap.Dispose();
+        }
+    }
+}
