9c77765e8774533880a72c2d13c29594df1062a1
Tomona Nanase <nanase@users.noreply.github.com>
2014-09-26 23:51:53 +0900

    TextRendererOptionsTest#LineHeightTestメソッドを追加

--- UnitTest/Graphics/TextRendererOptionsTest.cs
+++ UnitTest/Graphics/TextRendererOptionsTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nagisa.Graphics;
@@ -8,5 +9,19 @@ namespace UnitTest.Graphics
     [TestClass]
     public class TextRendererOptionsTest
     {
+        private const string NanoDigiPath = "./resource/nanodigi.ttf";
+
+        [TestMethod]
+        public void LineHeightTest()
+        {
+            const int LineHeight = 10;
+
+            using (FontLoader fontLoader = new FontLoader(NanoDigiPath))
+            using (Font font = new Font(fontLoader.Families[0], 10.0f))
+            using (TextRendererOptions options = new TextRendererOptions(font, LineHeight))
+            {
+                Assert.AreEqual(LineHeight, (int)options.LineHeight);
+            }
+        }
     }
 }
