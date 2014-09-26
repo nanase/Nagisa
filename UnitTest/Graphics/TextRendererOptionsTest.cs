f98e25678edee5ca71204201327a113f09b6822f
Tomona Nanase <nanase@users.noreply.github.com>
2014-09-26 22:13:41 +0900

    TextRendererOptionsTestクラスを追加

---
+++ UnitTest/Graphics/TextRendererOptionsTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nagisa.Graphics;
+
+namespace UnitTest.Graphics
+{
+    [TestClass]
+    public class TextRendererOptionsTest
+    {
+    }
+}
